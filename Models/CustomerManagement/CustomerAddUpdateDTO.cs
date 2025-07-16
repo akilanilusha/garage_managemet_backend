7924d9090c18af3f1442874a13ad0cbed39ceaec
DNishadini <Dinushikaherath150@gmail.com>
2025-07-16 23:55:16 +0530

    Update CustomerAddUpdateDTO.cs

--- Models/CustomerManagement/CustomerAddUpdateDTO.cs
+++ Models/CustomerManagement/CustomerAddUpdateDTO.cs
@@ -2,6 +2,7 @@
 
 public class CustomerAddUpdateDTO
 {
+    public string Id { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string ContactInfo { get; set; } = string.Empty;
